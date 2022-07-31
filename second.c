7ac89f00eb0d2b347de62c14af0941ca828879db
Sandarpan Mukherjee <sandarpan.mukherjee@gmail.com>
2022-07-31 17:25:04 +0530

    Shifted curly braces down

--- second.c
+++ second.c
@@ -2,7 +2,8 @@
 #include <cs50.h>
 #include <string.h>
 
-int main(void){
+int main(void)
+{
     string s = get_string("Enter text: ");
     printf("You have entered: \"%s\"\n", s);
     printf("The string length is %li\n", strlen(s));
